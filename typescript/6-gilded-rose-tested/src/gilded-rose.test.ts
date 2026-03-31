b99c5c5f8442422c61abdcfd3cc25f57888f3bf5
Ferdinand Ade <ferdinand.ade@codecentric.de>
2026-03-31 21:56:56 +0200

    add missing tests to ts

--- typescript/6-gilded-rose-tested/src/gilded-rose.test.ts
+++ typescript/6-gilded-rose-tested/src/gilded-rose.test.ts
@@ -150,4 +150,64 @@ describe('Gilded Rose', () => {
     expect(app.items[0].sellIn).toBe(-1);
     expect(app.items[0].quality).toBe(0);
   });
+
+  it('increases Backstage passes quality by one when sellIn is above 10', () => {
+    const items = [new ItemBuilder().called('Backstage passes to a TAFKAL80ETC concert').toBeSoldIn(11).ofQuality(10).build()];
+    const app = new GildedRose(items);
+
+    app.updateQuality();
+
+    expect(app.items[0].sellIn).toBe(10);
+    expect(app.items[0].quality).toBe(11);
+  });
+
+  it('never increases Backstage passes quality above 50 when sellIn is 10 or less', () => {
+    const items = [new ItemBuilder().called('Backstage passes to a TAFKAL80ETC concert').toBeSoldIn(10).ofQuality(49).build()];
+    const app = new GildedRose(items);
+
+    app.updateQuality();
+
+    expect(app.items[0].sellIn).toBe(9);
+    expect(app.items[0].quality).toBe(50);
+  });
+
+  it('increases Backstage passes quality twice when sellIn is between 6 and 10', () => {
+    const items = [new ItemBuilder().called('Backstage passes to a TAFKAL80ETC concert').toBeSoldIn(6).ofQuality(10).build()];
+    const app = new GildedRose(items);
+
+    app.updateQuality();
+
+    expect(app.items[0].sellIn).toBe(5);
+    expect(app.items[0].quality).toBe(12);
+  });
+
+  it('never increases Backstage passes quality above 50 when sellIn is 5 or less', () => {
+    const items = [new ItemBuilder().called('Backstage passes to a TAFKAL80ETC concert').toBeSoldIn(5).ofQuality(49).build()];
+    const app = new GildedRose(items);
+
+    app.updateQuality();
+
+    expect(app.items[0].sellIn).toBe(4);
+    expect(app.items[0].quality).toBe(50);
+  });
+
+  it('does not lower quality twice as fast on last day of sale', () => {
+    const items = [new ItemBuilder().called('Any Item').toBeSoldIn(1).ofQuality(10).build()];
+    const app = new GildedRose(items);
+
+    app.updateQuality();
+
+    expect(app.items[0].sellIn).toBe(0);
+    expect(app.items[0].quality).toBe(9);
+  });
+
+  it('never increases Aged Brie quality above 50 when past expiration date', () => {
+    const items = [new ItemBuilder().called('Aged Brie').pastExpirationDate().ofQuality(49).build()];
+    const app = new GildedRose(items);
+
+    app.updateQuality();
+
+    expect(app.items[0].sellIn).toBe(-2);
+    expect(app.items[0].quality).toBe(50);
+  });
 });
